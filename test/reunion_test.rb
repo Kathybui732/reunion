f23a7775b7c19aa43023dcd77515aad9e9761951
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-01 18:25:01 -0600

    Create test for exists and attributes for reunion

---
+++ test/reunion_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/reunion"
+
+class ReunionTest < MiniTest::Test
+
+	def setup
+    @reunion = Reunion.new("1406 BE")
+    @activity_1 = Activity.new("Brunch")
+	end
+
+	def test_it_exists_with_attributes
+		assert_instance_of Reunion, @reunion
+    assert_equal "1406 BE", @reunion.name
+	end
+
+end
