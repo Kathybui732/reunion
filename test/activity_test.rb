7b124dc9ea93a5971a990e7da6838f7b9f128882
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-01 18:14:06 -0600

    Fix test for total cost

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -23,6 +23,10 @@ class ActivityTest < MiniTest::Test
   end
 
   def test_total_cost
+    @activity.add_participant("Maria", 20)
     assert_equal 20, @activity.total_cost
+    @activity.add_participant("Luther", 40)
+    assert_equal ({"Maria" => 20, "Luther" => 40}), @activity.participants
+    assert_equal 60, @activity.total_cost
   end
 end
